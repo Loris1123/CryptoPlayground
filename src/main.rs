fd9586b73e4ead9f6849b3b372bfb69902bc52fe
Loris1123 <molottermann@gmail.com>
2016-09-10 16:34:35 +0200

    Create an LFSR and initialize it with random values

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+extern crate rand;
+
+use rand::Rng;
+
 fn main(){
-    println!("Hello    CryptoPlayground");
+    println!("Welcome to CryptoPlayground");
+    println!("Start LFSR");
+    lfsr();
+}
+
+fn lfsr(){
+    // Create the 7-bit-register
+    let mut register = [0; 7];
+
+    // Initialize it with values between 0 and 1
+    for (i,j) in register.iter_mut().enumerate() {
+        *j = rand::thread_rng().gen_range(0, 2);
+    }
+    println!("Register: {:?}", register);
+
+    
 }
