322457f53fbe5aaec0fa5702caa19c6092162f93
Loris1123 <molottermann@gmail.com>
2016-09-12 13:41:41 +0200

    use commandline arguments to specify which part of the playground you want to run

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,33 @@
+use std::env;
+
 mod lfsr;
 mod feistel;
 
 fn main(){
     println!("Welcome to CryptoPlayground");
-    println!("Running LFSR");
-    lfsr::lfsr();
+    let args: Vec<String> = env::args().collect();
 
-    println!("Running Feistel");
-    feistel::feistel();
+    if args.len() == 1{
+        print_helptext();
+        return;
+    }
+
+    for arg in &args[1..]{
+        match arg.as_ref() {
+            "--lfsr" => lfsr::lfsr(),
+            "--feistel" => feistel::feistel(),
+            "--help" => print_helptext(),
+            _ => print_helptext(),
+        }
+    }
+}
+
+fn print_helptext(){
+    println!("CryptoPlayground");
+    println!("");
+    println!("Specify what you want to do by choosing one of the followng arguments");
+    println!("The following arguments are available");
+    println!("--help\tPrint this helptext");
+    println!("--lfsr\tStart the Linear-Feedback-Shift-Register");
+    println!("--feistel\tStart the Feistelcrypt");
 }
