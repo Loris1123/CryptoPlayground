2ab2825ec54198fd855dcd5754e1b5067c8705e5
Loris1123 <molottermann@gmail.com>
2016-09-12 12:59:22 +0200

    LFSR: remove unnecessary code

--- src/lfsr.rs
+++ src/lfsr.rs
@@ -2,6 +2,13 @@ extern crate rand;
 
 use self::rand::Rng;
 
+
+/// A Linear-Feedback-Shift-Register.
+///
+/// Start with a random Initialization Vector.
+/// Then 1st bit will be taken as output.
+/// XOR with 3rd and 7th bit.
+/// The result will be used as a new input and the register is shifted 1 bit to the left
 pub fn lfsr(){
     // Create the 7-bit-register
     let mut register = [0; 7];
@@ -13,16 +20,12 @@ pub fn lfsr(){
     println!("Register: {:?}", register);
 
     for i in 1..20{
-        // Get Important bits:
-        let first   = register[0];
-        let third   = register[2];
-        let seventh = register[6];
-
-        // Calculate the new input
-        let result  = (first ^ third) ^ seventh;
+        // Calculate the new input-bit
+        let result  = (register[0] ^ register[2]) ^ register[6];
 
         // Shift
         // Following code is not beautiful, but easy to understand:
+        println!("Output: {}", register[0]);
         register[0] = register[1];
         register[1] = register[2];
         register[2] = register[3];
@@ -30,7 +33,6 @@ pub fn lfsr(){
         register[4] = register[5];
         register[5] = register[6];
         register[6] = result;
-        println!("Output: {}", first);
         println!("Register after iteration {}: {:?}", i, register);
     }
 }
