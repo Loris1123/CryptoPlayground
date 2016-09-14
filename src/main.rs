eed49965b09f0155c15829ee41219c47daf9c94f
Loris1123 <molottermann@gmail.com>
2016-09-14 20:00:07 +0200

    Parse commandline arguments by using getopts

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
+extern crate getopts;
+
 use std::env;
+use getopts::Options;
 
 mod lfsr;
 mod feistel;
@@ -7,28 +10,23 @@ fn main(){
     println!("Welcome to CryptoPlayground");
     let args: Vec<String> = env::args().collect();
 
-    if args.len() == 1{
-        print_helptext();
-        return;
-    }
+    let mut opts = Options::new();
+    opts.optflag("h", "help", "Print this helptext");
+    opts.optflag("", "feistel", "Start Feistel encryption");
+    opts.optflag("", "lfsr", "Start LFSR");
 
-    for arg in &args[1..]{
-        // Skip the first argument, because it is the name of the binary.
-        match arg.as_ref() {
-            "--lfsr" => lfsr::lfsr(12),
-            "--feistel" => feistel::encrypt(),
-            "--help" => print_helptext(),
-            _ => print_helptext(),
-        }
-    }
-}
+    let matches = match opts.parse(&args[1..]){
+        Ok(m) => m,
+        Err(f) => panic!("Error: {}",f),
+    };
 
-fn print_helptext(){
-    println!("CryptoPlayground");
-    println!("");
-    println!("Specify what you want to do by choosing one of the followng arguments");
-    println!("The following arguments are available");
-    println!("--help\tPrint this helptext");
-    println!("--lfsr\tStart the Linear-Feedback-Shift-Register");
-    println!("--feistel\tStart the Feistelcrypt");
+    if matches.opt_present("h"){
+        print!("{}", opts.usage(""));
+    }
+    if matches.opt_present("feistel"){
+        feistel::encrypt();
+    }
+    if matches.opt_present("lfsr"){
+        lfsr::lfsr(15);
+    }
 }
